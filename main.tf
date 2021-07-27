88166e8642117b47ddbb722b9918463117f6e093
Conor Murphy <conmurph@cisco.com>
2021-07-28 00:31:05 +0200

    initial

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ variable "applications" {
 
 module "aci_single_bd_single_epg" {
   for_each =  toset(var.applications)
-  source = "../modules/tfe_aci_single_bd_single_epg"
+  source = "github.com/conmurphy/tf_module_tfe_aci_single_bd_single_epg.git"
   application_name = each.key
   oauth_token_id = var.oauth_token_id
   tfe_organization_name = var.tfe_organization_name
--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
+variable "github_token" {}
 variable "tfcb_token" {}
 variable "oauth_token_id" {}
-variable "github_token" {}
 variable "tfe_organization_name" {}
 variable "tfe_agent_pool_name" {}
